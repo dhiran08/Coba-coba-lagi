0c57ea5a52b450953025e590b540e0e57fc97c14
Dhiran08 <dhiranandi08@gmail.com>
2019-01-28 06:43:02 +0700

    first commit

---
+++ dua.js
@@ -0,0 +1,25 @@
+// PERCOBAAN 1
+// require('./satu.js');
+
+// PERCOBAAN 2
+// var siapa = require('./satu');
+// console.log (siapa);
+
+//PERCOBAAN 3
+// var siapa = require('./satu');
+// console.log(siapa('Andi'));
+
+//PERCOBAAN 4
+// var data = require('./satu');
+// console.log(data.nama);
+// console.log(data.usia);
+
+//PERCOBAAN 5
+// var data = require('./satu');
+// console.log(data.nama);
+// console.log(data.usia);
+
+//PERCOBAAN 6
+var data = require('./satu');
+console.log(data.nama);
+console.log(data.usia);
---
+++ satu.js
@@ -0,0 +1,31 @@
+//PERCOBAAN 1
+// var nama = 'Andi';
+// console.log(nama);
+
+// Percobaan 2
+// var nama = 'Andi';
+// module.exports = nama;
+
+//PERCOBAAN 3
+// var nama = (x) => {
+//     return nama = x + ' Susilo'
+// };
+// module.exports = nama;
+
+//PERCOBAAN 4
+// var nama = 'Andi';
+// var usia = 26;
+// module.exports.nama = nama;
+// module.exports.usia = usia;
+
+//PERCOBAAN 5
+// module.exports.nama = 'Andi';
+// module.exports.usia = 26;
+
+//PERCOBAAN 6
+var nama = 'Andi';
+var usia = 26;
+module.exports = {
+    nama: nama,
+    usia: usia
+}
